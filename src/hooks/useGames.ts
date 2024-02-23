82cdff86f5cfe84b3fda95b95d17cd83e678beaa
markdeavey <marcosdelbasso@hotmail.com>
2024-02-23 23:29:45 +0100

    Updating some code

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,27 +11,30 @@ const useGames = () => {
   return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     queryFn: ({ pageParam = 1 }) =>
-      apiClient.getAll({
-        params: {
-          genres: gameQuery.genreId,
-          parent_platforms: gameQuery.platformId,
-          ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText,
-          page: pageParam,
-        },
-      }).then(response => {
-        // Filtrar los juegos que tengan background_image !== null
-        const filteredGames = response.results.filter(game => game.background_image !== null);
-        return {
-          ...response,
-          results: filteredGames,
-        };
-      }),
+      apiClient
+        .getAll({
+          params: {
+            genres: gameQuery.genreId,
+            parent_platforms: gameQuery.platformId,
+            ordering: gameQuery.sortOrder,
+            search: gameQuery.searchText,
+            page: pageParam,
+          },
+        })
+        .then((response) => {
+          const filteredGames = response.results.filter(
+            (game) => game.background_image !== null
+          );
+          return {
+            ...response,
+            results: filteredGames,
+          };
+        }),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    initialPageParam: 1, // Add initialPageParam here
-    staleTime: ms("24hs"), // 24 hours
+    initialPageParam: 1, 
+    staleTime: ms("24hs"), 
   });
 };
 
