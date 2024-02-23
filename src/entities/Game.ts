8926b0d7a581c2449de80e789456a51094a5361e
markdeavey <marcosdelbasso@hotmail.com>
2024-02-23 11:42:29 +0100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,8 +1,12 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
+  publishers: Publisher[];
+  genres: Genre[];
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,8 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
 import ExpandableText from "../components/ExpandableText";
+import GameAttributes from "../components/GameAttributes";
+import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -14,7 +15,8 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <ExpandableText >{game.description_raw}</ExpandableText>
+      <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
